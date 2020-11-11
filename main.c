5a103a5228a3e5c12eeaa1380e045b2d8d213085
luukaszh <lu_kaszh@wp.pl>
2020-11-11 15:39:20 +0100

    Add function main

---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>  //biblioteka zawierająca standardowe funkcje wejścia/wyjścia
+
+int main() {  //funkcja inicjująca pracę programu
+ /* instrukcje do wykonania */
+ return 0;  //przerwanie
+}
